9d071dcc3e0dcf4251d395bee2467f3399db7fa3
Juan Arenas <arenas.juan.f@gmail.com>
2023-04-05 00:06:27 -0700

    Defined order_split_required and prepare_split_data; moved sku_to_pouches to config.py

--- order_splitter.py
+++ order_splitter.py
@@ -1,14 +1,11 @@
+import base64
 import json
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import config
 
-sku_to_pouches = {
-    'SKU1': 1,
-    'SKU2': 2,
-    'SKU3': 3,
-    # ... more SKUs
-}
+auth_string = f"{config.SHIPSTATION_API_KEY}:{config.SHIPSTATION_API_SECRET}"
+encoded_auth_string = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
 
 def lambda_handler(event, context):
     print("Lambda handler invoked")
@@ -17,7 +14,7 @@ def lambda_handler(event, context):
     session = requests.Session()
     session.headers.update({
         "Content-Type": "application/json",
-        "Authorization": f"Basic {config.encoded_auth_string}"
+        "Authorization": f"Basic {encoded_auth_string}"
     })
 
     # Extract webhook payload
@@ -25,11 +22,11 @@ def lambda_handler(event, context):
     # Modify the resource_url to includeShipmentItems=True to access the order items
     response = session.get(payload['resource_url'][:-5] + "True")
     data = response.json()
-    shipments = data['shipments']
+    shipments = data['orders']
     print(f"Number of shipments: {len(shipments)}")
 
     with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(process_order, shipment, session) for shipment in shipments]
+        futures = [executor.submit(process_order, shipment) for shipment in shipments]
         for future in as_completed(futures):
             future.result()
 
@@ -39,27 +36,32 @@ def lambda_handler(event, context):
     }
 
 def process_order(shipment):
-    order = shipment['order']  # Assuming 'order' contains the order information
+    order = shipment  # Extract order information from the shipment
 
     if order_split_required(order):
         original_order, child_orders = prepare_split_data(order)
-        original_order_id = original_order['orderId']
 
         # Update the original order with reduced quantities
-        update_original_order(session, original_order_id, original_order)
+        update_original_order(original_order)
 
         # Create new child orders
         for child_order in child_orders:
-            create_child_order(session, child_order)
+            create_child_order(child_order)
 
-def update_original_order(order_id, order_data):
-    url = f'https://ssapi.shipstation.com/orders/{order_id}'
-    response = session.put(url, json=order_data)
+    # Format order items list as a string
+    order_items = ', '.join([f"{item['sku']}({item['quantity']})" for item in order['items']])
+
+    # Add the formatted order items string to the order object
+    order['customerNotes'] = f"Items: {order_items}"
+
+def update_original_order(order_data):
+    url = 'https://ssapi.shipstation.com/orders/createorder'
+    response = session.post(url, json=order_data)
 
     if response.status_code == 200:
-        print(f"Original order {order_id} updated successfully")
+        print(f"Original order {order_data['orderId']} updated successfully")
     else:
-        print(f"Error updating original order {order_id}: {response.text}")
+        print(f"Error updating original order {order_data['orderId']}: {response.text}")
 
 def create_child_order(child_order):
     url = 'https://ssapi.shipstation.com/orders/createorder'
@@ -70,4 +72,29 @@ def create_child_order(child_order):
     else:
         print(f"Error creating child order: {response.text}")
 
-# Include the order_split_required and prepare_split_data functions here
+def order_split_required(order):
+    total_pouches = sum([item['quantity'] * config.sku_to_pouches.get(item['sku'], 0) for item in order['items']])
+    return total_pouches > 9
+
+def prepare_split_data(order):
+    original_order = order.copy()
+    child_orders = []
+    remaining_pouches = sum([item['quantity'] * config.sku_to_pouches.get(item['sku'], 0) for item in order['items']])
+
+    while remaining_pouches > 9:
+        child_order = order.copy()
+        child_order_items = []
+
+        for item in order['items']:
+            pouches_per_item = config.sku_to_pouches.get(item['sku'], 0)
+            while item['quantity'] > 0 and remaining_pouches > 9:
+                item['quantity'] -= 1
+                remaining_pouches -= pouches_per_item
+                child_order_items.append({'sku': item['sku'], 'quantity': 1})
+
+        child_order['items'] = child_order_items
+        child_orders.append(child_order)
+
+    original_order['items'] = [{'sku': item['sku'], 'quantity': item['quantity']} for item in order['items']]
+
+    return original_order, child_orders
